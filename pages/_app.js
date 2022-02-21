54ccea75eb93b2b27b1015b6c87ff8a8532ee205
Khush <kchandawat@gmail.com>
2022-02-21 19:08:16 +0530

    new page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#fec409", //yellow
+      200: "#1a1b1f", //text - color
+      500: "#fb7f09", //orange
+      600: "#BF5709", //dark orange
+      700: "#822f08", //brand dark orange
+      800: "#230c0f", //brown siena
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
